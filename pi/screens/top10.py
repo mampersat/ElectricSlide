2662881e4b8138c0b6a463ff466eb9cc32d103b0
Christopher Flynn <cflynn@dyn.com>
2016-11-15 00:53:41 +0000

    Add actual ip address at the bottom

--- pi/screens/top10.py
+++ pi/screens/top10.py
@@ -1,3 +1,4 @@
+import socket
 from datetime import datetime
 import pygame
 from pygame.mixer import Sound
@@ -42,8 +43,10 @@ class Top10(LcarsScreen):
                         layer=1)
         all_sprites.add(LcarsBlockLarge(colours.BEIGE, (249, 16), "LAST SLIDE"),
                         layer=1)
-        all_sprites.add(LcarsText(colours.BLACK, (444, 612), "192 168 0 3"),
-                        layer=1)
+
+        # Get ip address of machine
+        ip = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
+        all_sprites.add(LcarsText(colours.BLACK, (444, 612), ip), layer=1)
 
         # Leader Board
         rows = get_leaderboard()
