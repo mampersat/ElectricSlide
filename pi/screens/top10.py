e2db549a706c4d5bcf567a2bb637e474c32a2f11
Christopher Flynn <cflynn@dyn.com>
2016-11-15 19:03:46 +0000

    Added try/except around ip address bits

--- pi/screens/top10.py
+++ pi/screens/top10.py
@@ -42,8 +42,11 @@ class Top10(LcarsScreen):
         all_sprites.add(LcarsBlockLarge(colours.BEIGE, (249, 16), "LAST SLIDE", self.last_gadget), layer=1)
 
         # Get ip address of machine
-        ip = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
-        all_sprites.add(LcarsText(colours.BLACK, (444, 612), ip), layer=1)
+        try:
+            ip = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
+            all_sprites.add(LcarsText(colours.BLACK, (444, 612), ip), layer=1)
+        except:
+            all_sprites.add(LcarsText(colours.BLACK, (444, 612), "No Network Connection"), layer=1)
         self.leaders = pygame.sprite.Group()
 
         # date display
